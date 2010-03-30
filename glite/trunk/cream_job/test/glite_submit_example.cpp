b41711e09093a8d3ba7e44beba11224c8198066f
oweidner <oweidner@bd306f8e-b875-4415-8b40-16139df0eb6e>
2010-03-30 20:44:09 +0000

    fixed example
    git-svn-id: file://localhost/tmp/svn2git/saga-wg/svn@54 bd306f8e-b875-4415-8b40-16139df0eb6e

--- glite/trunk/cream_job/test/glite_submit_example.cpp
+++ glite/trunk/cream_job/test/glite_submit_example.cpp
@@ -1,7 +1,22 @@
 #include <saga/saga.hpp>
 
-saga::job::description jd;
-jd.set_attribute(saga::job::attributes::description_executable, "/bin/date");
 
-saga::job::service js("cream://cream-09.pd.infn.it:8443/cream-pbs-cream_A");
-saga::job::job j = js.create_job(jd);
+int main(int argc, char **argv)
+{
+
+  saga::job::description jd;
+
+  jd.set_attribute (saga::job::attributes::description_executable, "/bin/date/");
+
+  saga::job::service js("cream://cream-09.pd.infn.it:8443/cream-pbs-cream_A");
+  saga::job::job cream_job = js.create_job(jd);
+ 
+  cream_job.run(); 
+  
+  std::cout << "\nJob ID    : " << cream_job.get_job_id() << std::endl;
+  std::cout << "Job State : "   << cream_job.get_state() << std::endl;
+  
+  cream_job.wait(-1.0);         // waits for state change
+  
+  std::cout << "Job State : "   << cream_job.get_state() << std::endl;
+}
