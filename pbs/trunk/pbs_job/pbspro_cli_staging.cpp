19f009f81029f34489b6fea8dba281ac658a487d
svn_goiwai <svn_goiwai@bd306f8e-b875-4415-8b40-16139df0eb6e>
2010-05-27 07:40:47 +0000

    first commit in svn.cct.lsu.edu: pbspro adaptor for job
    git-svn-id: file://localhost/tmp/svn2git/saga-wg/svn@145 bd306f8e-b875-4415-8b40-16139df0eb6e

---
+++ pbs/trunk/pbs_job/pbspro_cli_staging.cpp
@@ -0,0 +1,191 @@
+/*
+ * Copyright (C) 2008-2009 High Energy Accelerator Research Organization (KEK)
+ * Copyright (C) 2008-2009 National Institute of Informatics in Japan.
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <iostream>
+
+#include <boost/algorithm/string.hpp>
+#include <boost/foreach.hpp>
+
+#include <saga/impl/exception.hpp>
+#include <saga/saga/adaptors/file_transfer_spec.hpp>
+#include <saga/saga/url.hpp>
+
+#include "pbspro_cli_staging.hpp"
+
+namespace pbspro_job { namespace cli {
+
+  //////////////////////////////////////////////////////////////////////
+  //
+  class file_transfer_impl : public file_transfer {
+
+  private:
+    transfer_ptr t;
+    staging type;
+    creation mode;
+
+  public:
+    //
+    file_transfer_impl(std::string source, std::string target,
+		       staging s, creation c = overwrite) :
+      t(new transfer(source, target)), type(s), mode(c) {}
+    //
+    DESTRUCTOR(file_transfer_impl);
+    //
+    staging get_type() const { return type; }
+    //
+    transfer_ptr get_transfer() const { return t; }
+    //
+    std::string get_source() const { return t->first; }
+    //
+    std::string get_target() const { return t->second; }
+  };
+
+  //////////////////////////////////////////////////////////////////////
+  //
+  bool file_transfer_parser_impl::check_filename(std::string name) const {
+    saga::url file(name);
+
+    if (!file.get_scheme().empty() || !file.get_host().empty()) {
+      return false;
+    }
+
+#if 0
+    std::cout << "path=" << file.get_path() << std::endl;
+
+    boost::filesystem::path l(name);
+    std::cout << "l.root_name=" << l.root_name() << std::endl;
+    std::cout << "l.root_directory=" << l.root_directory() << std::endl;
+    std::cout << "l.relative_path=" << l.relative_path() << std::endl;
+#endif
+
+    return true;
+  }
+
+  //////////////////////////////////////////////////////////////////////
+  //
+  file_transfer_ptr file_transfer_parser_impl::parse(std::string spec) const {
+    std::string left, right;
+    saga::adaptors::file_transfer_operator mode;
+
+    if (!parse_file_transfer_specification(spec,
+					   left, mode, right)) {
+      SAGA_OSSTREAM strm;
+      strm << "Parse failed: "
+	   << "(FileTransfer entry: '" << spec << "').";
+      SAGA_ADAPTOR_THROW_NO_CONTEXT(SAGA_OSSTREAM_GETSTRING(strm),
+				    saga::BadParameter);
+      throw;
+    }
+#if 0
+    if (!check_filename(left) || !check_filename(right)) {
+      SAGA_OSSTREAM strm;
+      strm << "Cannot handle the specified URL "
+	   << "(FileTransfer entry: '" << spec << "').";
+      SAGA_ADAPTOR_THROW_NO_CONTEXT(SAGA_OSSTREAM_GETSTRING(strm),
+				    saga::IncorrectURL);
+    }
+#endif
+
+    file_transfer_ptr p;
+    switch (mode) {
+    case saga::adaptors::copy_local_remote:
+      p = file_transfer_ptr(new file_transfer_impl(left, right,
+						   file_transfer::in));
+      break;
+    case saga::adaptors::copy_remote_local:
+      p = file_transfer_ptr(new file_transfer_impl(right, left,
+						   file_transfer::out));
+      break;
+    case saga::adaptors::append_local_remote:
+    case saga::adaptors::append_remote_local:
+      {
+	SAGA_OSSTREAM strm;
+	strm << "Append operation is unsupported "
+	     << "(FileTransfer entry: '" << spec << "').";
+	SAGA_ADAPTOR_THROW_NO_CONTEXT(SAGA_OSSTREAM_GETSTRING(strm),
+				      saga::NotImplemented);
+      }
+      break;
+    case saga::adaptors::unknown_mode:
+    default:
+      {
+	// from condor
+	SAGA_OSSTREAM strm;
+	strm << "Unknown FileTransfer operator "
+	     << "(FileTransfer entry: '" << spec << "').";
+	SAGA_ADAPTOR_THROW_NO_CONTEXT(SAGA_OSSTREAM_GETSTRING(strm),
+				      saga::BadParameter);
+      }
+      break;
+    }
+
+    return p;
+  }
+
+  //////////////////////////////////////////////////////////////////////
+  // resolve path for job execution host
+  //
+  std::string staging_path_builder::resolve_local(const std::string& path)
+  {
+    boost::filesystem::path _path(path);
+#if 0
+    if (_path.empty()) {
+      // throw
+    }
+#endif
+    if (_path.has_root_directory()) {
+      return path;
+    }
+
+    boost::filesystem::path x = workdir / _path;
+    x.normalize();
+    return x.string();
+  }
+
+  // resolve path for qsub execution host
+  std::string staging_path_builder::resolve_remote(const std::string& path)
+  {
+    boost::filesystem::path _path(path);
+
+    std::ostringstream os;
+    os << remotehost << ":";
+#if 0
+    if (_path.empty()) {
+      // throw
+    }
+#endif
+
+    if (_path.has_root_directory()) {
+      os << path;
+    } else {
+      boost::filesystem::path x = boost::filesystem::current_path();
+      x /= _path;
+      x.normalize();
+      os << x.string();
+    }
+    return os.str();
+  }
+
+  //
+  std::string staging_path_builder::get_cwd()
+  {
+    boost::filesystem::path cwd = boost::filesystem::current_path();
+    return cwd.string();
+  }
+
+}}
---
+++ pbs/trunk/pbs_job/pbspro_helper.hpp
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2008-2009 High Energy Accelerator Research Organization (KEK)
+ * Copyright (C) 2008-2009 National Institute of Informatics in Japan.
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef TORQUE_HELPER_HPP
+#define TORQUE_HELPER_HPP
+
+#include <boost/regex.hpp>
+
+#include <saga/saga/attribute.hpp>
+#include <saga/saga/job.hpp>
+
+#include "pbspro_job_adaptor.hpp"
+
+#define RE_PBS_JOBID   "((\\d+(-\\d+)?)(\\.(\\S+))?)"
+#define RE_SAGA_JOBID  "^\\[([^\\]]+)\\]-\\[([^\\]]+)\\]$"
+
+namespace pbspro_job { namespace helper {
+
+  // baseid : 231.kek-sna131.soum.co.jp
+  //  pbsid : 231.kek-sna131.soum.co.jp : true
+  //        : 231.kek-sna131            : true
+  //        : 231                       : true
+  //        : 231.kek-sna132            : false
+  //        : 232                       : false
+  inline
+  bool pbsid_match(std::pair<std::string, saga::job::description> entry,
+		   std::string baseid) {
+    //std::cout << "entry.first:" << entry.first << std::endl;
+    //std::cout <<      "baseid:" << baseid << std::endl;
+    // Sorry, under construction.
+    return false;
+  }
+
+  //////////////////////////////////////////////////////////////////////
+  // helper class
+
+  class jobid_converter {
+    saga::url backend_url;
+    boost::regex re_jobid;
+    boost::regex re_pbsid;
+  public:
+    //
+    jobid_converter() {}
+    jobid_converter(saga::url rm) :
+      backend_url(rm),
+      re_jobid(RE_SAGA_JOBID), re_pbsid(RE_PBS_JOBID)
+    {}
+
+    ////////////////////////////////////////////////////////////////////
+    // convert pbs-id to job-id
+    std::string convert_jobid(const std::string& pbsid);
+
+    ////////////////////////////////////////////////////////////////////
+    // convert job-id to pbs-id
+    std::string convert_pbsid(const std::string& sagaid);
+
+    ////////////////////////////////////////////////////////////////////
+	// convert job-id to pbs-id , short version (only returns job#)
+	std::string convert_pbsid_short(const std::string& sagaid);
+
+    ////////////////////////////////////////////////////////////////////
+    // get server_host from SAGA JobID
+	std::string get_server_host(const std::string& sagaid);
+
+  };
+
+  //////////////////////////////////////////////////////////////////////
+  // helper functions
+  bool create_saga_job_description(saga::job::description& jd,
+				   std::string cmd,
+				   std::string host);
+  //
+  bool split_command_line(std::string cmd,
+			  std::string& executable,
+			  std::vector<std::string>& arguments);
+  //
+  saga::job::state convert_saga_job_state(std::string pbspro_status);
+
+}}
+
+#endif  // TORQUE_HELPER_HPP
